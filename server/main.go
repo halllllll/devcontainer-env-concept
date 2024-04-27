0299b84ab562a995bde6dd2a65476395a385d70c
h4lllllll <hjod1172@yahoo.co.jp>
2024-04-27 08:51:47 +0000

    use parent git in each devcontainer

--- server/main.go
+++ server/main.go
@@ -6,8 +6,14 @@ import (
 )
 
 func main() {
+
 	http.HandleFunc("/", echoHello)
+	http.HandleFunc("/hello", hello)
 	http.ListenAndServe(":8085", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello!</h1")
 }
 
 func echoHello(w http.ResponseWriter, r *http.Request) {
